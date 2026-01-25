7aa85ae1d15500d18306f74372813e78271fd996
Vichers <lozex283@gmail.com>
2026-01-25 17:08:16 +0300

    исправил ошибки, обновил docker compose

--- MainService/src/main/java/ru/aston/GlobalExceptionHandler.java
+++ MainService/src/main/java/ru/aston/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package ru.aston;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,12 +11,19 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
-    public String handleCircuitBreakerException(CircuitBreakerException circuitBreakerException) {
-        return circuitBreakerException.getMessage();
+    public String handleCircuitBreakerException(CircuitBreakerException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleCircuitBreakerException(MethodArgumentNotValidException e) {
+        return e.getMessage();
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public void handleException(Throwable e) {
+    public String handleException(Throwable e) {
+        return e.getMessage();
     }
 }
